d4391a6c115763b7851c25568881000b8b31c291
CB006597 CB006597 <CB006597@students.apiit.lk>
2018-01-12 15:08:44 +0530

    Added HtmlAgilityPack

---
+++ Data/Repository/DataAccess/SiteGrabber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Data.Model;
+
+namespace Data.Repository.DataAccess
+{
+    public class SiteGrabber
+    {
+        private string _url;
+
+        public SiteGrabber(string url)
+        {
+            _url = url;
+        }
+
+        public SiteEntity Site()
+        {
+            var siteEntity = new SiteEntity();
+
+            return siteEntity;
+        }
+    }
+}
